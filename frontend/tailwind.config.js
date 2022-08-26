f233a805c990ca55fcbfc377eb8beb1a4b520a48
pranavvdesai <pranavdesai.psd@gmail.com>
2022-08-26 18:28:58 +0530

    fix: tailwind and other config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};

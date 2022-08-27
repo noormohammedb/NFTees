f08d830304116c114fe9786454f3630d12f9e346
Harsh Singh <harshsingh16d@gmail.com>
2022-08-27 19:34:21 +0530

    Remove old addresses from config.js. Update deployjs

--- src/blockchain/scripts/deploy.js
+++ src/blockchain/scripts/deploy.js
@@ -1,26 +1,18 @@
 const hre = require("hardhat");
 const fs = require('fs');
 
-async function main() {
-  const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
-  const MockERC20 = await hre.ethers.getContractFactory("MockERC20");
-  const MyToken = await hre.ethers.getContractFactory("MyToken");
-  const nftMarketplace = await NFTMarketplace.deploy();
-  const mockERC20 = await MockERC20.deploy();
-  const myToken = await MyToken.deploy();
-  await nftMarketplace.deployed();
-  await mockERC20.deployed();
-  await myToken.deployed();
-  console.log("nftMarketplace deployed to:", nftMarketplace.address);
-  console.log("erc20 deployed to:", mockERC20.address);
-  console.log("myToken deployed to:", myToken.address);
+async function main () {
+  const NFTEE = await hre.ethers.getContractFactory("NFTEE");
+  const nftee = await NFTEE.deploy();
+  await nftee.deployed();
+ 
+  console.log("NFTEE deployed to:", nftee.address);
+  
 
   fs.writeFileSync(
     "./config.js",
     `
-  export const marketplaceAddress = "${nftMarketplace.address}"
-  export const erc20Address = "${mockERC20.address}"
-  export const myTokenAddress = "${myToken.address}"
+    export const nftAddress = "${nftee.address}";
   `
   );
 }

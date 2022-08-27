e0e297fa6a82bbd90ef959e2280f6207de5812b3
Harsh Singh <harshsingh16d@gmail.com>
2022-08-27 21:28:17 +0530

    modify navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
 import CreateNewButton from "./CreateNewButton";
 import NavDropDown from "./DropDown";
@@ -10,76 +10,55 @@ import SearchForm from "./SearchForm";
 import useStickyHeader from "./StickyHeader";
 
 export default function Header() {
-    const brandLogo = "img/core-img/logo.png";
-    const darkLogo = "img/core-img/logo-white.png";
+  const brandLogo = "img/core-img/logo.png";
+  const darkLogo = "img/core-img/logo-white.png";
 
-    let [check] = useState(true);
-    const sticky = useStickyHeader(10);
-    const stickyClass = `${(sticky && check) ? 'sticky-on' : ''}`
+  let [check] = useState(true);
+  const sticky = useStickyHeader(10);
+  const stickyClass = `${sticky && check ? "sticky-on" : ""}`;
 
-    return (
-        <header className={`header-area ${stickyClass}`} >
-            <Navbar expand="lg">
-                <Container>
-                    {/* Navbar Brand */}
-                    <Link className="navbar-brand" to="/">
-                        <img className="light-logo" src={`${process.env.PUBLIC_URL}/${brandLogo}`} alt="Light" />
-                        <img className="dark-logo" src={`${process.env.PUBLIC_URL}/${darkLogo}`} alt="Dark" />
-                    </Link>
+  return (
+    <header className={`header-area ${stickyClass}`}>
+      <Navbar expand="lg">
+        <Container>
+          {/* Navbar Brand */}
+          <Link className="navbar-brand" to="/">
+            <img
+              className="light-logo"
+              src={`${process.env.PUBLIC_URL}/${brandLogo}`}
+              alt="Light"
+            />
+            <img
+              className="dark-logo"
+              src={`${process.env.PUBLIC_URL}/${darkLogo}`}
+              alt="Dark"
+            />
+          </Link>
 
-                    {/* Navbar Toggler */}
-                    <Navbar.Toggle className="navbar-toggler" aria-controls="funtoNav" />
+          {/* Navbar Toggler */}
+          <Navbar.Toggle className="navbar-toggler" aria-controls="funtoNav" />
 
-                    {/* Navbar */}
-                    <Navbar.Collapse id="funtoNav">
-                        {/* Navbar List */}
-                        <FuntoNavbar />
+          {/* Navbar */}
+          <Navbar.Collapse id="funtoNav">
+            {/* Navbar List */}
+            <FuntoNavbar />
 
-                        {/* Header Meta */}
-                        <div className="header-meta d-flex align-items-center ms-lg-auto">
+            {/* Header Meta */}
+            <div className="header-meta d-flex align-items-center ms-lg-auto">
+              <NavLink to="/dashboard" className="ft-dd" title="Home" id="Home">
+                Dashboard
+              </NavLink>
 
-                            {/* Search Form */}
-                            <SearchForm />
-
-                            {/* User Dropdown */}
-                            <NavDropDown
-                                dropdownID="dropdownMenuButton1"
-                                toggleIcon="bi-three-dots"
-                                dropdownList={[
-                                    {
-                                        text: "Dashboard",
-                                        url: "/dashboard",
-                                        icon: "bi-speedometer2"
-                                    },
-                                    {
-                                        text: "Collections",
-                                        url: "/collections",
-                                        icon: "bi-collection"
-                                    },
-                                    {
-                                        text: "Notifications",
-                                        url: "/notifications",
-                                        icon: "bi-bell"
-                                    },
-                                    {
-                                        text: "Settings",
-                                        url: "/settings",
-                                        icon: "bi-gear"
-                                    }
-                                ]}
-                            />
-
-                            {/* Create New Button */}
-                            <CreateNewButton
-                                buttonColor="btn-warning"
-                                buttonURL="/create-new"
-                                buttonText="Create New"
-                            />
-
-                        </div>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    )
+              {/* Create New Button */}
+              <CreateNewButton
+                buttonColor="btn-warning"
+                buttonURL="/create-new"
+                buttonText="Create New"
+              />
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }
--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -6,7 +6,6 @@ export default function FuntoNavbar() {
             <NavLink to="/home1" className="ft-dd" title="Home" id="Home">Home</NavLink>
             <NavLink to="/dashboard" className="ft-dd" title="Home" id="Home">Dashboard</NavLink>
             <NavLink to="/create-new" className="ft-dd" title="Home" id="Home">Mint NFT</NavLink>
-            <NavLink to="/home1" className="ft-dd" title="Home" id="Home">Home</NavLink>
            
         </div>
     )

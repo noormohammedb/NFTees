1c97bd26a88582ae37e64939a11df0e63ea7802c
Harsh Singh <harshsingh16d@gmail.com>
2022-08-27 21:33:26 +0530

    modified navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 

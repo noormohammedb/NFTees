5c0a46dc65aa265d632a628b7a3344be1f41c239
pranavvdesai <pranavdesai.psd@gmail.com>
2022-08-27 13:12:10 +0530

    feat: erc20

--- src/blockchain/scripts/deploy.js
+++ src/blockchain/scripts/deploy.js
@@ -3,13 +3,26 @@ const fs = require('fs');
 
 async function main() {
   const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const MockERC20 = await hre.ethers.getContractFactory("MockERC20");
+  const MyToken = await hre.ethers.getContractFactory("MyToken");
   const nftMarketplace = await NFTMarketplace.deploy();
+  const mockERC20 = await MockERC20.deploy();
+  const myToken = await MyToken.deploy();
   await nftMarketplace.deployed();
+  await mockERC20.deployed();
+  await myToken.deployed();
   console.log("nftMarketplace deployed to:", nftMarketplace.address);
+  console.log("erc20 deployed to:", mockERC20.address);
+  console.log("myToken deployed to:", myToken.address);
 
-  fs.writeFileSync('./config.js', `
+  fs.writeFileSync(
+    "./config.js",
+    `
   export const marketplaceAddress = "${nftMarketplace.address}"
-  `)
+  export const erc20Address = "${mockERC20.address}"
+  export const myTokenAddress = "${myToken.address}"
+  `
+  );
 }
 
 main()
